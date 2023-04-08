9de999e1b6f84dcc15198cb882956d1c8fc7ddfa
danielyusril <danielyusril24@gmail.com>
2023-04-08 10:08:48 +0700

    Pembuatan backend halaman registrasi, login, dan membuat frontend dari halaman reservasi pasien baru dan lama

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,31 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Hash;
+use DB;
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'nama' => 'User RS',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('user'),
+            'role' => 'pasien'
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert([
+            'nama' => 'Admin RS',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin'),
+            'role' => 'admin'
+        ]);
     }
 }
