e944a091787556661361d9498ef8acf84fd229ce
friscaalifiaa <friscakamila@gmail.com>
2023-05-17 10:57:21 +0700

    Membuat halaman melihat list dokter #15, Membuat halaman update data dokter untuk admin #16, Membuat halaman delete dokter untuk admin #17

--- resources/views/pages/admin/master-dokter.blade.php
+++ resources/views/pages/admin/master-dokter.blade.php
@@ -101,7 +101,6 @@ function showSuccess($success)
     </div>
     </div>
 
-    
     @if(count($errors) > 0)
             <div class="alert alert-danger alert-dismissible fade show" role="alert">
               <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
